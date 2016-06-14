e701bbe09ae3d11455dbb8f1fb87f3800cff630b
tanner.cherry <tanner.cherry@gmail.com>
2016-06-14 12:07:57 -0500

    Made manager class for account

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
 class Account(AbstractBaseUser):
@@ -27,3 +28,28 @@ class Account(AbstractBaseUser):
 
     def get_short_name(self):
         return self.first_name
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username.')
+
+        account = self.model(
+            email=self.normalize_email(email), username=kwargs.get('username')
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
