370ac7dbf91b2f7529a5d697dbc526837904ccb5
tanner.cherry <tanner.cherry@gmail.com>
2016-06-15 10:12:56 -0500

    Added an API endpoint

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,8 +2,19 @@ from django.conf.urls import patterns, url
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
     '',
+
+    url('^.*$', IndexView.as_view(), name='index'),
+
+    url(r'^api/vl/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
