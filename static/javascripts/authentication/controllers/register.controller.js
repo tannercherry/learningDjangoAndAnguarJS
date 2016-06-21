66bbd8d59388ce0cc836375d3df4e95111c3de41
tanner.cherry <tanner.cherry@gmail.com>
2016-06-21 14:18:41 -0500

    Made login page to see if it would work, still no page after finishing

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -7,13 +7,33 @@
 
   RegisterController.$inject = ['$location', '$scope', 'Authentication'];
 
+  activate();
+
+  function activate() {
+    if (Authentication.isAuthenticated()) {
+      $location.url('/');
+    }
+  }
+
   function RegisterController($location, $scope, Authentication) {
     var vm = this;
 
     vm.register = register;
 
-    function register() {
-      Authentication.register(vm.email, vm.password, vm.username);
+    function register(email, password, username) {
+      return $http.post('/api/v1/accounts/', {
+        username: username,
+        password: password,
+        email: email
+      }).then(registerSuccessFn, registerErrorFn);
+
+      function registerSuccessFn(data, status, headers, config) {
+        Authentication.login(email, password);
+      }
+
+      function registerErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      }
     }
   }
 })();
--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -10,7 +10,12 @@
   function Authentication($cookies, $http) {
 
     var Authentication = {
-      register: register
+      getAuthenticatedAccount: getAuthenticatedAccount,
+      isAuthenticated: isAuthenticated,
+      login: login,
+      register: register,
+      setAuthenticatedAccount: setAuthenticatedAccount,
+      unauthenticate: unauthenticate
     };
 
     return Authentication;
@@ -22,5 +27,35 @@
         email: email
       });
     }
+    function login(email, password) {
+      return $http.post('/api/v1/auth/login/', {
+        email: email, password: password
+      }).then(loginSuccessFn, loginErrorFn);
+
+      function loginSuccessFn(data, status, headers, config) {
+        Authentication.setAuthenticatedAccount(data.data);
+
+        window.location = '/';
+      }
+
+      function loginErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      }
+    }
+    function getAuthenticatedAccount() {
+      if (!$cookies.authenticatedAccount) {
+        return;
+      }
+      return JSON.parse($cookies.authenticatedAccount);
+    }
+    function isAuthenticated() {
+      return !!$cookies.authenticatedAccount;
+    }
+    function setAuthenticatedAccount(account) {
+      $cookies.authenticatedAccount = JSON.stringify(account);
+    }
+    function unauthenticate() {
+      delete $cookies.authenticatedAccount;
+}
   }
 })();
